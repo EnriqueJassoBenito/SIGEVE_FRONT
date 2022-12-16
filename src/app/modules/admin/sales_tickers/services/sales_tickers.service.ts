037cb51a52a555c37b1ca6469100a5e2e834e688
EnriqueJassoBenito <20213tn033@utez.edu.mx>
2022-12-15 18:15:27 -0600

    El fin de Pochopolis

--- src/app/modules/admin/sales_tickers/services/sales_tickers.service.ts
+++ src/app/modules/admin/sales_tickers/services/sales_tickers.service.ts
@@ -1,45 +1,19 @@
-import {Injectable} from "@angular/core";
-import {Sale_ticker} from "../types/sales_tickers";
-import {APP_URL} from "../../../../services/base-url-app";
-import {HttpClient} from "@angular/common/http";
-
-@Injectable({
-  providedIn: 'root'
-})
-
-export class Sales_tickersService{
-  loading: boolean = false;
-  private sale_tickerArray: Sale_ticker[] = []
-  edit: boolean = false
-
-  get shows() {
-    return [...this.sale_tickerArray]
-  }
-  constructor(private http: HttpClient) {
-  }
-
-  findAll(){
-    this.loading = true
-    return this.http.get<Sale_ticker[]>(`${APP_URL}api/salestickets/all`)
-  }
-}
-
 // import {Injectable} from "@angular/core";
 // import {Sale_ticker} from "../types/sales_tickers";
-// import {HttpClient} from "@angular/common/http";
 // import {APP_URL} from "../../../../services/base-url-app";
+// import {HttpClient} from "@angular/common/http";
 //
 // @Injectable({
 //   providedIn: 'root'
 // })
 //
-// export class Sales_tickersService {
+// export class Sales_tickersService{
 //   loading: boolean = false;
 //   private sale_tickerArray: Sale_ticker[] = []
 //   edit: boolean = false
 //   sale_ticker: Sale_ticker = {
 //     movie_show_ste: {},
-//     client_spo: {},
+//     name_usr: {},
 //     total_count: 0,
 //   }
 //
@@ -54,3 +28,34 @@ export class Sales_tickersService{
 //     return this.http.get<Sale_ticker[]>(`${APP_URL}api/salestickets/all`)
 //   }
 // }
+//
+// // import {Injectable} from "@angular/core";
+// // import {Sale_ticker} from "../types/sales_tickers";
+// // import {HttpClient} from "@angular/common/http";
+// // import {APP_URL} from "../../../../services/base-url-app";
+// //
+// // @Injectable({
+// //   providedIn: 'root'
+// // })
+// //
+// // export class Sales_tickersService {
+// //   loading: boolean = false;
+// //   private sale_tickerArray: Sale_ticker[] = []
+// //   edit: boolean = false
+// //   sale_ticker: Sale_ticker = {
+// //     movie_show_ste: {},
+// //     client_spo: {},
+// //     total_count: 0,
+// //   }
+// //
+// //   get shows() {
+// //     return [...this.sale_tickerArray]
+// //   }
+// //   constructor(private http: HttpClient) {
+// //   }
+// //
+// //   findAll(){
+// //     this.loading = true
+// //     return this.http.get<Sale_ticker[]>(`${APP_URL}api/salestickets/all`)
+// //   }
+// // }
--- src/app/modules/admin/sales_tickers/types/sales_tickers.ts
+++ src/app/modules/admin/sales_tickers/types/sales_tickers.ts
@@ -1,6 +1,4 @@
 import {Entity} from "../../../../types/entity";
-import {Movie} from "../../movies/types/movie";
-import {Room} from "../../rooms/types/rooms";
 import {Movie_show} from "../../movies_shows/types/movies_shows";
 import {UserSigning} from "../../../auth/types/user";
 
@@ -8,5 +6,5 @@ export type Sale_ticker = Entity<number> &{
   id_ste?: number;
   movie_show_ste?: Movie_show;
   client_spo?: UserSigning;
-  total_count?: string;
+  total_count?: number;
 }
